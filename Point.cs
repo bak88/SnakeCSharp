bfc20195bfe8df1a6add430955a058b5a275b499
bak88 <dimbak1990@gmail.com>
2024-09-03 22:02:01 +0300

    Snake3

--- Point.cs
+++ Point.cs
@@ -8,5 +8,14 @@ namespace Snake
 {
     internal class Point
     {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,25 +8,18 @@
             p1.x = 1;
             p1.y = 3;
             p1.sym = '*';
-            Draw(p1.x, p1.y, p1.sym);
+            p1.Draw();
 
             Point p2 = new();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
-            Draw(p1.x, p1.y, p1.sym);
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Draw(); 
+            //
 
-            Draw(x2, y2, sym2);
+            
         }
 
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.WriteLine(sym);
-
-        }
+       
     }
 }
